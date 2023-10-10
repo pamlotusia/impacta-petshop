75e8ecb209bb9e9ad2f8afc914d1865c227a2838
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-10-09 21:20:59 -0300

    navbar estilizado e funcionando falta agora: - adicionar função de sair ao botao - criar rotas das paginas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -9,14 +9,15 @@ function classNames(...classes) {
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
+    <div className='w-full h-20 flex  justify-between items-center px-8 text-white bg-[#0A082E] font-poppins'>
+      <h1 className='text-2xl font-medium ml-20'>Pelos&Plumas</h1>
+      <ul className='flex mr-20'>
         <li>
         <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Options
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        <Menu.Button className="flex w-full justify-center items-center px-3 py-2 text-md">
+          agendamentos
+          <ChevronDownIcon className="mx-3 h-5 w-5 text-white" aria-hidden="true" />
         </Menu.Button>
       </div>
 
@@ -37,10 +38,78 @@ const Navbar = () => {
                   href="#"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    'block px-4 py-2 text-md'
                   )}
                 >
-                  Account settings
+                  Histórico
+                </a>
+              )}
+            </Menu.Item>
+            
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-md'
+                  )}
+                >
+                  Novo agendamento
+                </a>
+              )}
+            </Menu.Item>
+
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+        </li>
+
+        <li>
+        <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="flex w-full justify-center items-center gap-x-1. px-3 py-2 text-md">
+          perfil
+          <ChevronDownIcon className="mx-3 h-5 w-5 text-white" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-md'
+                  )}
+                >
+                  Meu perfil
+                </a>
+              )}
+            </Menu.Item>
+            
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-md'
+                  )}
+                >
+                  Cadastrar pet
                 </a>
               )}
             </Menu.Item>
@@ -50,23 +119,10 @@ const Navbar = () => {
                   href="#"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    'block px-4 py-2 text-md'
                   )}
                 >
-                  Support
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  License
+                  Meus pets
                 </a>
               )}
             </Menu.Item>
@@ -77,10 +133,10 @@ const Navbar = () => {
                     type="submit"
                     className={classNames(
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full px-4 py-2 text-left text-sm'
+                      'block w-full px-4 py-2 text-left text-md'
                     )}
                   >
-                    Sign out
+                    Sair
                   </button>
                 )}
               </Menu.Item>
@@ -90,6 +146,7 @@ const Navbar = () => {
       </Transition>
     </Menu>
         </li>
+
       </ul>
     </div>
   )
