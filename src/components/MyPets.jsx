5103470af9ab3c62c1bff709a36d88c1f9f8b1ec
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-10-21 23:29:45 -0300

    conexão com o banco porem ta com bugg

--- src/components/MyPets.jsx
+++ src/components/MyPets.jsx
@@ -1,38 +1,92 @@
-import React from 'react'
-import iconDog from '../images/icon-dog.svg'
-import iconCat from '../images/icon-cat.svg'
-import iconBird from '../images/icon-bird.svg'
-import iconRodent from '../images/icon-rodent.svg'
+import React, { useEffect, useState } from 'react';
+import { auth } from '../firebase';
+import { get, getDatabase, ref } from 'firebase/database';
+
+// Importe os ícones diretamente
+import iconDog from '../images/icon-dog.svg';
+import iconCat from '../images/icon-cat.svg';
+import iconBird from '../images/icon-bird.svg';
+import iconRodent from '../images/icon-rodent.svg';
 
 const MyPets = () => {
+  const [pets, setPets] = useState([]);
+
+  const colorIcon = {
+    dog: {
+      color: 'medium-blue',
+      icon: iconDog,
+    },
+    cat: {
+      color: 'yellow',
+      icon: iconCat,
+    },
+    bird: {
+      color: 'green',
+      icon: iconBird,
+    },
+    rodent: {
+      color: 'pink',
+      icon: iconRodent,
+    },
+  };
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const user = auth.currentUser;
+        const uid = user.uid;
+
+        const db = getDatabase();
+        const petsRef = ref(db, `users/${uid}/pets`);
+
+      const snapshot = await get(petsRef);
+
+        if (snapshot.exists()) {
+          const petData = snapshot.val();
+          const petArray = Object.values(petData);
+          setPets(petArray);
+        } else {
+          console.log('Nenhum dado de pets encontrado.');
+        }
+      } catch (error) {
+        console.log('Erro ao buscar dados: ', error);
+      }
+    };
+    fetchPets();
+  }, []);
+
   return (
-    <div>
-      <p className="font-poppins text-3xl text-center color-title mb-[60px] my-10">
-        Meus <span className="font-bold">bichinhos</span>
-      </p>
-      <div id="board" className="m-20 grid grid-cols-5 gap-y-10">
-        <div className="flex flex-col">
-          <div className="w-[260px] min-h-[300px] bg-[#F7F7F9] rounded-2xl mx-10 custom-shadow">
+    
+    <div className="m-20 grid grid-cols-5 gap-y-1">
+      {pets.map((pet, index) => {
+        const petType = pet.tipoDeAnimal;
+        const petInfo = colorIcon[petType];
+
+        if (!petInfo) {
+          return null;
+        }
+
+        return (
+          <div className={`w-[260px] min-h-[300px] bg-${petInfo.color} rounded-2xl mx-10 custom-shadow`} key={index}>
             <div className="flex flex-col items-center">
-              <div className="h-[130px] w-[130px] mt-10 flex justify-center items-center rounded-full yellow ">
-                <img src={iconDog} alt="" className="h-[100px] w-[100px] " />
+              <div className={`${petInfo.color} h-[130px] w-[130px] mt-10 flex justify-center items-center rounded-full`}>
+                <img src={petInfo.icon} alt={pet.nomePet} className="h-[100px] w-[100px]" />
               </div>
-              <p className="w-full text-lg  text-center font-medium break-words px-3 mt-2 ">
-                Nome
+              <p className="w-full text-lg text-center font-medium break-words px-3 mt-2 ">
+                {pet.nomePet}
               </p>
-              <p>Idade</p>
-              <p>Peso</p>
-              <a href="" className="yellow-font mt-4 uppercase font-medium ">
+              <p>Idade: {pet.idadePet}</p>
+              <p>Peso: {pet.pesoPet}</p>
+              <a href="" className="yellow-font mt-4 uppercase font-medium">
                 {' '}
                 abrir
               </a>
             </div>
           </div>
-        </div>
-      </div>
-      
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default MyPets
+export default MyPets;
