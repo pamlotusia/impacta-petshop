7ee2dcc54b35b47e677ebb9b1caf0c15ff72209e
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-02-19 20:34:01 -0300

    layout sidebar pronto e logout funcionando

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   const{user} = UserAuth()
   const userEmail = user? user.email : null
   console.log(userEmail)
-  const employeeEmails = ['funcionario@gmail.com']
+  const employeeEmails = ['employee@gmail.com', 'funcionario@gmail.com']
 
   const isEmployee = employeeEmails.includes(userEmail)
   console.log(isEmployee)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
-import { AiFillHome } from "react-icons/ai";
+import { UserAuth } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+// icons
+import { IoHome } from 'react-icons/io5'
+import { IoCalendar } from 'react-icons/io5'
+import { FaMoneyBillTransfer } from 'react-icons/fa6'
+import { IoIosExit } from 'react-icons/io'
 
 const Sidebar = () => {
+  const { logout } = UserAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logout()
+      navigate('/')
+      console.log('you are logout')
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+
   return (
-    <div className='p-5 bg-background-color w-[30%] color-title'>
-      <p className='font-medium text-1xl '>Pelos&Plumas</p>
-      <div className='flex items-center' >
-      <AiFillHome size={40}/>
-      <p className='font-medium'>Home</p>
+    <div className="p-6 pt-[10%] bg-background-color w-[20%] h-screen fixed left-0 top-0 color-title">
+      <div className="flex items-center mb-8 cursor-pointer">
+        <IoHome size={40} />
+        <p className="font-medium ml-6">Home</p>
+      </div>
+
+      <div className="flex items-center mb-8 cursor-pointer">
+        <IoCalendar size={40} />
+        <p className="font-medium ml-6">Agendamentos</p>
+      </div>
+
+      <div className="flex items-center cursor-pointer">
+        <FaMoneyBillTransfer size={40} />
+        <p className="font-medium ml-6">Financeiro</p>
+      </div>
+
+      <div className="flex items-center mt-[50%] light-grey-font cursor-pointer" onClick={handleLogout}>
+        <IoIosExit size={40} />
+        <p className="font-medium ml-6">Sair</p>
       </div>
     </div>
   )
