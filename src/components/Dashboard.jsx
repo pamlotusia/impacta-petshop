1826733495c1b6d848d0c207beb8e2fb54a05c4a
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-02-28 19:59:56 -0300

    commit para mudar de branch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Cards from './Cards'
 
 const Dashboard = () => {
   return (
     
     <div className='flex justify-center h-screen top-0 bg-gray-200 ml-[20%] w-[80%]'>
       <p>Home</p>
-      
+      <Cards/>
     </div>
   )
 }
